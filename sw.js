056bab602a1ad1e2eece6151d0fbf5b6cb7cd9d6
aristihernandez-svg <aristihernandez@gmail.com>
2026-08-09 20:18:22 -0400

    Use real Navajo logo image for the Navajo procedure tile
    Matches the existing pattern for metro/westwind/astra in PROC_IMAGES
    instead of falling back to the generic plane emoji.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'skycare-yav-v5';
+const CACHE = 'skycare-yav-v6';
 const SHELL = ['./', './index.html', './manifest.json', './baseConfig.js'];
 
 self.addEventListener('install', e => {
