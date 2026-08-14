cbb134ca4ce3e4dd922ad366b74c9358731313a8
aristihernandez-svg <aristihernandez@gmail.com>
2026-08-13 23:54:09 -0400

    Remove Jessie easter egg from YAV Home hero
    Same class of leftover personal content as the earlier Emerson
    removal -- a July 23 date-check swapping in a specific person's
    photo, copied over from YKF's file along with everything else.
    No asset folder existed in this repo, only the code reference.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'skycare-yav-v12';
+const CACHE = 'skycare-yav-v13';
 const SHELL = ['./', './index.html', './manifest.json', './baseConfig.js'];
 
 self.addEventListener('install', e => {
