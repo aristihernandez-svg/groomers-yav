dee6edb4e6cfc3876600c8f5f96a911b4956b20f
aristihernandez-svg <aristihernandez@gmail.com>
2026-08-13 22:59:52 -0400

    Show battery/engine problem detail on the individual car page
    Same fix as YKF -- red banner with battery status/DTC codes on the
    car detail page, and 'critical' added to the battery-alert condition.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'skycare-yav-v10';
+const CACHE = 'skycare-yav-v11';
 const SHELL = ['./', './index.html', './manifest.json', './baseConfig.js'];
 
 self.addEventListener('install', e => {
